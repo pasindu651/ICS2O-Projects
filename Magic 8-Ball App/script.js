fa014ce786397110e919998703cca34eff830b64
Pasindu Danthanarayana <65776520+pasindu651@users.noreply.github.com>
2022-04-08 09:23:49 -0400

    Update script.js

--- Magic 8-Ball App/script.js
+++ Magic 8-Ball App/script.js
@@ -1,8 +1,8 @@
-      /*Create Magic eight ball*/
+      //Create Magic eight ball
       var canvas = document.getElementById("myCanvas");
       var ctx = canvas.getContext("2d");
 
-      /*Create gradient*/
+      //Create gradient
       var grd = ctx.createLinearGradient(7, 0, 200, 0);
       grd.addColorStop(0, "black");
       grd.addColorStop(1, "#545353");
@@ -11,37 +11,37 @@
       grd.addColorStop(0, "black");
       grd.addColorStop(1, "#545353");
 
-      /*First circle*/
+      //First circle
       ctx.beginPath();
       ctx.arc(245, 250, 210, 0, 2 * Math.PI);
       ctx.fillStyle = grd;
       ctx.fill()
 
-      /*Second circle*/
+      //Second circle
       ctx.beginPath();
       ctx.arc(245, 250, 110, 0, 2 * Math.PI);
       ctx.fillStyle = '#000000';
       ctx.fill()
 
-      /*Triangle*/
+      //Triangle
       ctx.beginPath();
       ctx.moveTo(244.5,360);
       ctx.shadowBlur = 80;
       ctx.shadowColor = "blue";
 
-      /*Base*/
+      //Base
       ctx.lineTo(343,200);
       ctx.lineTo(146,200);
       ctx.fillStyle = "blue";
       ctx.fill();
 
-      /*Ask question*/
+      //Ask question
       userQuest = prompt("Please ask me a question. I shall give you the answer.");
 
-      /*Generate random number*/
+      //Generate random number
       randomNum = Math.floor(8 * Math.random() + 1);
 
-      /*Pick random number*/
+      //Pick random number
       if (randomNum == 1) {
         canvas.insertAdjacentHTML('beforebegin', "<p id='response'>It is certain.</p>");
       } else if (randomNum == 2) {
